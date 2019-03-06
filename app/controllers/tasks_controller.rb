3b9bbc43fdbca402b721dd288fb4ea8c44a69b39
nami.takeda <lose.lily.nami@gmail.com>
2019-03-06 09:43:53 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,57 +1,59 @@
 class TasksController < ApplicationController
-    def index
-      @tasks = Task.all
-    end
+  before_action :set_task, only: [:show, :edit, :update, :delete]
+  
+  def index
+    @tasks = Task.all
+  end
       
-    def show
-      @task = Task.find(params[:id])
-    end
+  def show
+  end
     
-    def new
-      @task = Task.new
-    end
+  def new
+    @task = Task.new
+  end
     
-    def create
-      @task = Task.new(task_parameter)
+  def create
+    @task = Task.new(task_parameter)
       
-      if @task.save
-        flash[:success] = 'タスクが正常に投稿されました'
-        redirect_to @task
-      else
-        flash.now[:danger] = 'タスクが投稿されませんでした'
-        render :new
-      end
+    if @task.save
+      flash[:success] = 'タスクが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが投稿されませんでした'
+      render :new
     end
+  end
     
-    def edit
-      @task = Task.find(params[:id])
-    end
+  def edit
+  end
     
-    def update
-      @task = Task.find(params[:id])
+  def update
       
-      if @task.update(task_parameter)
-        flash[:success] = 'タスクが正常に更新されました'
-        redirect_to @task
-      else
-        flash.now[:danger] = 'タスクが更新されませんでした'
-        render :edit
-      end
+    if @task.update(task_parameter)
+      flash[:success] = 'タスクが正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが更新されませんでした'
+      render :edit
     end
+  end
     
-    def destroy
-      @task = Task.find(params[:id])
-      @task.destroy
+  def destroy
+    @task.destroy
       
-      flash[:success] = 'タスクは正常に削除されました'
-      redirect_to tasks_url
-    end
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_to tasks_url
+  end    
       
     
 end
 
 private
 
+def set_task
+  @task = Task.find(params[:id])
+end
+
 def task_parameter
-  params.require(:task).permit(:content)
+  params.require(:task).permit(:content, :status)
 end
