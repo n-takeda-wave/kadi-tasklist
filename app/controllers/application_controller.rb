e4e72a6d34ea0c542ae55ec89234ae24d33eaa69
nami.takeda <lose.lily.nami@gmail.com>
2019-03-21 14:58:51 +0000

    add privaten

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   
   include SessionsHelper
   
+  private
+  
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
